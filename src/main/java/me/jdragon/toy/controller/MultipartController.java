58b02dbec818f3c9735d0b012a1fc643952f89ab
Jaeyong <koola97620@nate.com>
2020-01-18 10:48:05 +0900

    Initial commit

---
+++ src/main/java/me/jdragon/toy/controller/MultipartController.java
@@ -0,0 +1,14 @@
+package me.jdragon.toy.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author choijaeyong on 2020/01/18.
+ * @project toy
+ * @description
+ */
+
+@RestController
+public class MultipartController {
+
+}
---
+++ src/test/java/me/jdragon/toy/controller/MultipartControllerTest.java
@@ -0,0 +1,18 @@
+package me.jdragon.toy.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+
+/**
+ * @author choijaeyong on 2020/01/18.
+ * @project toy
+ * @description
+ */
+class MultipartControllerTest {
+
+  @BeforeEach
+  void setUp() {
+
+  }
+}
