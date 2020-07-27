52c363047129b78eddc2a964d29733696c29d8b0
Jaeyong <koola97620@nate.com>
2020-07-27 23:49:45 +0900

    설계

--- src/main/java/me/jdragon/toy/order/domain/Order.java
+++ src/main/java/me/jdragon/toy/order/domain/Order.java
@@ -1,9 +1,11 @@
 package me.jdragon.toy.order.domain;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import lombok.Getter;
+import me.jdragon.toy.product.domain.Product;
 import org.springframework.data.annotation.Id;
 
 /**
@@ -21,5 +23,6 @@ public class Order {
   private Orderer orderer;
   private Integer productId;
   private LocalDateTime orderDate;
+  private List<Product> productList;
 
 }
